8215827318c8971586608adddcb42877f7a4f9e8
marmooo <marmooo@users.noreply.github.com>
2024-05-06 00:00:21 +0900

    bump @marmooo/ttf2svg from 0.2.0 to 0.2.1

--- mod.js
+++ mod.js
@@ -1,7 +1,7 @@
 import { getLigatureMap } from "./ligature.js";
 import { compress, decompress } from "npm:wawoff2@2.0.1";
 import { Font, parse } from "npm:opentype.js@1.3.4";
-import { font2svgFont, ttf2svgFont } from "npm:@marmooo/ttf2svg@0.2.0";
+import { font2svgFont, ttf2svgFont } from "npm:@marmooo/ttf2svg@0.2.1";
 import svg2ttf from "npm:svg2ttf@6.0.3";
 import ttf2eot from "npm:ttf2eot@3.1.0";
 import ttf2woff from "npm:ttf2woff@3.0.0";
