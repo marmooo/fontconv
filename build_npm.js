f989a600eaec87e27746e5d2239a48c5c498331d
marmooo <marmooo@users.noreply.github.com>
2025-01-25 10:51:52 +0900

    0.0.4

--- build_npm.js
+++ build_npm.js
@@ -22,7 +22,7 @@ await build({
   },
   package: {
     name: "fontconv",
-    version: "0.0.3",
+    version: "0.0.4",
     description:
       "Convert and compress fonts (.ttf, .otf, .eot, .svg, .woff, .woff2).",
     license: "MIT",
